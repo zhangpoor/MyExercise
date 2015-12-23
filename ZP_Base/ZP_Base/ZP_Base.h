8c8941cba2aea8875815afbeeddfff39cd054b04
zhangpoor <334521055@qq.com>
2015-12-23 15:48:50 +0800

    创建ZP_Base

---
+++ ZP_Base/ZP_Base/ZP_Base.h
@@ -0,0 +1,13 @@
+//
+//  ZP_Base.h
+//  ZP_Base
+//
+//  Created by zhangpoor on 15/12/23.
+//  Copyright © 2015年 zhangpoor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZP_Base : NSObject
+
+@end
---
+++ ZP_Base/ZP_Base/ZP_Base.m
@@ -0,0 +1,13 @@
+//
+//  ZP_Base.m
+//  ZP_Base
+//
+//  Created by zhangpoor on 15/12/23.
+//  Copyright © 2015年 zhangpoor. All rights reserved.
+//
+
+#import "ZP_Base.h"
+
+@implementation ZP_Base
+
+@end
